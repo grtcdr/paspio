f278d437b0ba800dfca0ba5baa5045ca2b61eddc
grtcdr <ba.tahaaziz@gmail.com>
2021-11-29 00:33:06 +0100

    misc: return as many as received

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if let Some(password) = &args.get(1) {
-        let pool = paspio::get_pool_size(password);
-        let entropy = paspio::get_entropy(pool, password);
+    let mut args: Vec<String> = std::env::args().collect();
+    args.remove(0);
 
-        print!("Entropy: {:.2} bits", entropy);
+    if args.is_empty() {
+        print!("No password provided.");
+        return;
     }
 
-    print!("No password provided.");
+    for password in &args {
+        let pool = paspio::get_pool_size(password);
+        let entropy = paspio::get_entropy(pool, password);
+        println!("{:.2} bits", entropy);
+    }
 }
