5234f41901aa1f9fbbfefd2dbefd5105bb595823
grtcdr <ba.tahaaziz@gmail.com>
2021-11-28 04:31:02 +0100

    chore: move functions to a core module

---
+++ src/core.rs
@@ -0,0 +1,62 @@
+use regex::Regex;
+
+pub fn has_digits(pw: &String) -> bool {
+    if pw.chars().any(|x| x.is_digit(10)) {
+        return true;
+    }
+    {
+        false
+    }
+}
+
+pub fn has_lowercase(pw: &String) -> bool {
+    if pw.chars().any(|x| x.is_lowercase()) {
+        return true;
+    }
+    {
+        false
+    }
+}
+
+pub fn has_uppercase(pw: &String) -> bool {
+    if pw.chars().any(|x| x.is_uppercase()) {
+        return true;
+    }
+    {
+        false
+    }
+}
+
+pub fn has_symbols(pw: &String) -> bool {
+    let re = Regex::new(r#"["'`~!@#$%^&*()-_+=[{]}\|;:,.<>/?]"#).unwrap();
+    re.is_match(pw)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_has_symbols() {
+        let pw = String::from("4HIW5e!+MIzVpa5G");
+        assert_eq!(has_symbols(&pw), true);
+    }
+
+    #[test]
+    fn test_has_uppercase() {
+        let pw = String::from("uYqqtZiEsifi2Mvu");
+        assert_eq!(has_uppercase(&pw), true);
+    }
+
+    #[test]
+    fn test_has_lowercase() {
+        let pw = String::from("VhHw654jxZXJ8Xtj");
+        assert_eq!(has_lowercase(&pw), true);
+    }
+
+    #[test]
+    fn test_has_digits() {
+        let pw = String::from("tqvQLJZ7ESc6o10h");
+        assert_eq!(has_digits(&pw), true);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use regex::Regex;
+mod core;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let mut pool = 0;
     if args.len() > 1 {
         let password = &args[1];
         if password.is_empty() {
@@ -10,19 +9,21 @@ fn main() {
             return;
         }
 
-        if has_digits(&password) {
+        let mut pool = 0;
+
+        if core::has_digits(&password) {
             pool += 10;
         }
 
-        if has_lowercase(&password) {
+        if core::has_lowercase(&password) {
             pool += 26;
         }
 
-        if has_uppercase(&password) {
+        if core::has_uppercase(&password) {
             pool += 26;
         }
 
-        if has_symbols(&password) {
+        if core::has_symbols(&password) {
             pool += 32;
         }
 
@@ -33,66 +34,5 @@ fn main() {
     }
     {
         println!("No password provided.");
-    }
-}
-
-fn has_digits(pw: &String) -> bool {
-    if pw.chars().any(|x| x.is_digit(10)) {
-        return true;
-    }
-    {
-        false
-    }
-}
-
-fn has_lowercase(pw: &String) -> bool {
-    if pw.chars().any(|x| x.is_lowercase()) {
-        return true;
-    }
-    {
-        false
-    }
-}
-
-fn has_uppercase(pw: &String) -> bool {
-    if pw.chars().any(|x| x.is_uppercase()) {
-        return true;
-    }
-    {
-        false
-    }
-}
-
-fn has_symbols(pw: &String) -> bool {
-    let re = Regex::new(r#"["'`~!@#$%^&*()-_+=[{]}\|;:,.<>/?]"#).unwrap();
-    re.is_match(pw)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_has_symbols() {
-        let pw = String::from("4HIW5e!+MIzVpa5G");
-        assert_eq!(has_symbols(&pw), true);
-    }
-
-    #[test]
-    fn test_has_uppercase() {
-        let pw = String::from("uYqqtZiEsifi2Mvu");
-        assert_eq!(has_uppercase(&pw), true);
-    }
-
-    #[test]
-    fn test_has_lowercase() {
-        let pw = String::from("VhHw654jxZXJ8Xtj");
-        assert_eq!(has_lowercase(&pw), true);
-    }
-
-    #[test]
-    fn test_has_digits() {
-        let pw = String::from("tqvQLJZ7ESc6o10h");
-        assert_eq!(has_digits(&pw), true);
     }
 }
