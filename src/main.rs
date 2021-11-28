08e054ad904446ec18eeba49c903ea4ca17ae90f
grtcdr <ba.tahaaziz@gmail.com>
2021-11-28 02:54:31 +0100

    tests: add unit tests for every function

--- src/main.rs
+++ src/main.rs
@@ -67,3 +67,32 @@ fn has_symbols(pw: &String) -> bool {
     let re = Regex::new(r#"["'`~!@#$%^&*()-_+=[{]}\|;:,.<>/?]"#).unwrap();
     re.is_match(pw)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_has_symbols() {
+        let pw = String::from("4HIW5e!+MIzVpa5G");
+        assert_eq!(has_symbols(&pw), true);
+    }
+
+    #[test]
+    fn test_has_uppercase() {
+        let pw = String::from("uYqqtZiEsifi2Mvu");
+        assert_eq!(has_uppercase(&pw), true);
+    }
+
+    #[test]
+    fn test_has_lowercase() {
+        let pw = String::from("VhHw654jxZXJ8Xtj");
+        assert_eq!(has_lowercase(&pw), true);
+    }
+
+    #[test]
+    fn test_has_digits() {
+        let pw = String::from("tqvQLJZ7ESc6o10h");
+        assert_eq!(has_digits(&pw), true);
+    }
+}
