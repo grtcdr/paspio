a60077055ebbaeb50cebf3a2c9092b7e309316cc
grtcdr <ba.tahaaziz@gmail.com>
2021-11-28 22:26:12 +0100

    misc: refactor main

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() > 1 {
-        let password = &args[1];
-        if password.is_empty() {
-            print!("No password provided.");
-            return;
-        }
-
+    if let Some(password) = &args.get(1) {
         let pool = paspio::get_pool_size(password);
         let entropy = paspio::get_entropy(pool, password);
 
         print!("Entropy: {:.2} bits", entropy);
-    } else {
-        print!("No password provided.");
     }
+
+    print!("No password provided.");
 }
